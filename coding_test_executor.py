2c062fbd534475998bea4ac02483cab5f2f43654
seono park <pianist5555@gmail.com>
2022-11-21 21:35:02 +0900

    Feat: 테스트 결과 문자열 수정

--- coding_test_executor.py
+++ coding_test_executor.py
@@ -8,10 +8,9 @@ def execute_test(solution: Callable, params: List):
         output = param[1]
         answer = solution(param)
         if answer == output:
-            is_right.append(f'\033[34m테스트를 통과하였습니다.\033[0m')
+            is_right.append(f'   입력값 〉 {input}\n    기댓값 〉 {output}\n 실행 결과 〉 \033[34m테스트를 통과하였습니다.\033[0m')
         else:
-            is_right.append(f'\033[31m실행한 결괏값 {answer}이 기댓값 {output}과 다릅니다.\033[0m')
+            is_right.append(f'   입력값 〉 {input}\n    기댓값 〉 {output}\n 실행 결과 〉 \033[31m실행한 결괏값 {answer}이 기댓값 {output}과 다릅니다.\033[0m')
     
     for n, res in enumerate(is_right):
-        print(f'테스트 {str(n)} -> {res}')
-
+        print(f'테스트 {str(n+1)}\n {res}\n {"-"*60}')
